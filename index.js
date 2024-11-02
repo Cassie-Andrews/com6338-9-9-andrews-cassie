9b8d48bcd3f02d8ea976c634db61a00cc965a625
Cassie Andrews <candrews1@ufl.edu>
2024-11-02 15:24:35 -0400

    convert to arrow functions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 
 const weatherURL = 'https://api.openweathermap.org/data/2.5/weather';
-// API Key: a8b8566d914e7ee5f3e4973ebeb94b48
+    // API Key: a8b8566d914e7ee5f3e4973ebeb94b48
 
 const weatherAppDiv = document.getElementById('weather-app');
 const form = document.querySelector('form');
@@ -16,8 +16,8 @@ const input = document.getElementById('weather-search');
 const weatherDisplay = document.getElementById('weather');
 
 
-// TODO Convert a function declaration into a arrow function.
-form.onsubmit = function(e) {
+// Convert a function declaration into a arrow function.
+form.onsubmit = async (e) => {
     e.preventDefault();
 
     const userQuery = input.value.trim();
@@ -32,18 +32,18 @@ form.onsubmit = function(e) {
 // TODO Convert a promise-based function (a function call with .then) to instead use async and await.
     fetch(fetchURL)
         .then(function(response) {
-            // TODO Convert string concatenation to template literals and string interpolation.
+// TODO Convert string concatenation to template literals and string interpolation.
             console.log("Response Status:" + " " + response.status);
-            
             // HANDLE LOCATION NOT FOUND
-            if (response.status === 404) { // notify user location was not found
+            if (response.status === 404) { // notify user location not found
                 showLocationNotFound();
                 input.value = ''; // clear inpur value
                 return; // exit if not found
             }  
             // HANDLE LOCATION FOUND
-            return response.json(); // if user entered search term that is found, retrieve data
+            return response.json(); // if search term found, retrieve data
         })
+// TODO Convert a promise-based function (a function call with .then) to instead use async and await.
         .then(function(data) { // UPDATE DISPLAY
             if (data) {
                 console.log(data);
@@ -53,9 +53,8 @@ form.onsubmit = function(e) {
         });
 }
 
-// TODO Convert a function declaration into a arrow function.
-function showLocationNotFound() {
-    // create <h2>Location not found</h2> above the form
+// Convert a function declaration into a arrow function.
+const showLocationNotFound = () => {
     const h2 = document.createElement('h2');
     h2.textContent = 'Location not found';
     weatherDisplay.innerHTML = ''; // clear
@@ -63,21 +62,22 @@ function showLocationNotFound() {
 }
 
 
-// TODO Convert a function declaration into a arrow function.
-function updateDisplay(data) {
+// Convert a function declaration into a arrow function.
+const updateDisplay = (data) => {
     weatherDisplay.innerHTML = ''; // clear
-
+// TODO Convert some object-related code to use ES6 destructuring.
     const city = data.name; // city code
+// TODO Convert some object-related code to use ES6 destructuring.
     const country = data.sys.country; // country code
 // TODO Convert string concatenation to template literals and string interpolation.
     const mapLink = "https://www.google.com/maps/search/?api=1&query=" + data.coord.lat + "," + data.coord.lon; // google maps link to location
 // TODO Convert string concatenation to template literals and string interpolation.
     const weatherIcon = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png'; // weather icon representing current conditions
-    const weatherDescription = data.weather[0].description; // description of current weather
+    const weatherDescription = data.weather[0].description; // desc of current weather
     const currentTemp = data.main.temp; // actual temp
     const feelsLike = data.main.feels_like; // feels like temp
     
-    const dataTime = data.dt * 1000; // Time of data calculation, unix, UTC
+    const dataTime = data.dt * 1000; // time of data calculation, unix, UTC
     const date = new Date(dataTime); // time last updated   
     const timeString = date.toLocaleTimeString('en-US', {
         hour: 'numeric',
