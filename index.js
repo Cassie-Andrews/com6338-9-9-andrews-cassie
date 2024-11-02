a101863075cbd3133e36a377c61fc3c52396bf55
Cassie Andrews <candrews1@ufl.edu>
2024-11-02 14:59:34 -0400

    removed break elements

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Your code here
-
 // INSTRUCTIONS:
 // To complete this assignment, you will need to do each of the following AT LEAST ONCE in your code:
     // Convert a var declaration to const or let where appropriate.
@@ -17,36 +15,33 @@ const form = document.querySelector('form');
 const input = document.getElementById('weather-search');
 const weatherDisplay = document.getElementById('weather');
 
-// ON APP LOAD
-    // only the input field and search button should be visible
-    // this is already in the HTML
 
-// ON SUBMIT 
+// TODO Convert a function declaration into a arrow function.
 form.onsubmit = function(e) {
     e.preventDefault();
 
     const userQuery = input.value.trim();
-        console.log("User input:" + " " + userQuery);
-    
-        // construct the fetch URL
+// TODO Convert string concatenation to template literals and string interpolation.
+    console.log("User input:" + " " + userQuery);
+// TODO Convert string concatenation to template literals and string interpolation.   
     const queryString = '?units=imperial&appid=a8b8566d914e7ee5f3e4973ebeb94b48&q=' + userQuery;
+// TODO Convert string concatenation to template literals and string interpolation.
     const fetchURL = weatherURL + queryString;
-    // app should call the Open Weather API's current weather endpoint using the JS fetch API to obtain weather data based on location entered by the user
     
 
 // TODO Convert a promise-based function (a function call with .then) to instead use async and await.
     fetch(fetchURL)
         .then(function(response) {
+            // TODO Convert string concatenation to template literals and string interpolation.
             console.log("Response Status:" + " " + response.status);
             
-            // LOCATION NOT FOUND
-            if (response.status === 404) { 
-                // notify the user that the location was not found
+            // HANDLE LOCATION NOT FOUND
+            if (response.status === 404) { // notify user location was not found
                 showLocationNotFound();
                 input.value = ''; // clear inpur value
                 return; // exit if not found
             }  
-            // LOCATION FOUND
+            // HANDLE LOCATION FOUND
             return response.json(); // if user entered search term that is found, retrieve data
         })
         .then(function(data) { // UPDATE DISPLAY
@@ -58,6 +53,7 @@ form.onsubmit = function(e) {
         });
 }
 
+// TODO Convert a function declaration into a arrow function.
 function showLocationNotFound() {
     // create <h2>Location not found</h2> above the form
     const h2 = document.createElement('h2');
@@ -67,13 +63,15 @@ function showLocationNotFound() {
 }
 
 
-// DISPLAY CURRENT WEATHER INFO       
+// TODO Convert a function declaration into a arrow function.
 function updateDisplay(data) {
     weatherDisplay.innerHTML = ''; // clear
 
     const city = data.name; // city code
     const country = data.sys.country; // country code
+// TODO Convert string concatenation to template literals and string interpolation.
     const mapLink = "https://www.google.com/maps/search/?api=1&query=" + data.coord.lat + "," + data.coord.lon; // google maps link to location
+// TODO Convert string concatenation to template literals and string interpolation.
     const weatherIcon = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png'; // weather icon representing current conditions
     const weatherDescription = data.weather[0].description; // description of current weather
     const currentTemp = data.main.temp; // actual temp
@@ -86,6 +84,7 @@ function updateDisplay(data) {
         minute: '2-digit'
     })
 
+// TODO Convert string concatenation to template literals and string interpolation.
     console.log("City:" + " " + city);
     console.log("Country:" + " " + country);
     console.log("Map link:" + " " + mapLink);
@@ -98,47 +97,44 @@ function updateDisplay(data) {
 
 
     const locationDisplay = document.createElement('h2');
+// TODO Convert string concatenation to template literals and string interpolation.
     locationDisplay.textContent = city + ', ' + country;
     weatherDisplay.appendChild(locationDisplay);
+
 
     const mapLinkDisplay = document.createElement('a');
     mapLinkDisplay.href = mapLink;
     mapLinkDisplay.target = "_blank";
     mapLinkDisplay.textContent = "Click to view map";
     weatherDisplay.appendChild(mapLinkDisplay);
-        // <a href="" target="_BLANK"/a>
+
 
     const weatherIconDisplay = document.createElement('img');
     weatherIconDisplay.src = weatherIcon;
     weatherIconDisplay.innerText = 'Click to view map';
     weatherDisplay.appendChild(weatherIconDisplay);
-        // <img src="">
 
 
-// this one breaks the code when i change to let or const
-    var breakElement = document.createElement('br');
     const weatherDescriptionDisplay = document.createElement('p');
     weatherDescriptionDisplay.textContent = weatherDescription;
     weatherDisplay.appendChild(weatherDescriptionDisplay);
-    weatherDisplay.appendChild(breakElement);
-        // <p style="text-transform: capitalize;"> </p><br>
+
 
     const currentTempDisplay = document.createElement('p');
+// TODO Convert string concatenation to template literals and string interpolation.
     currentTempDisplay.textContent = 'Current: ' + currentTemp + '°F';
     weatherDisplay.appendChild(currentTempDisplay);
-        // <p>Current: 53.74° F</p>
 
-// this one breaks the code when i change to let or const
-    var breakElement = document.createElement('br');
+
     const feelsLikeDisplay = document.createElement('p');
+// TODO Convert string concatenation to template literals and string interpolation.
     feelsLikeDisplay.textContent = 'Feels like: ' + feelsLike + '°F';
     weatherDisplay.appendChild(feelsLikeDisplay);
-    weatherDisplay.appendChild(breakElement);
-        // <p>Feels like: 51.69° F</p><br>
+
 
     const lastUpdatedDisplay = document.createElement('p');
+// TODO Convert string concatenation to template literals and string interpolation.
     lastUpdatedDisplay.textContent = 'Last updated: ' + timeString;
     weatherDisplay.appendChild(lastUpdatedDisplay);
-        // <p>Last updated: 11:00 PM</p>
     ;
 }
